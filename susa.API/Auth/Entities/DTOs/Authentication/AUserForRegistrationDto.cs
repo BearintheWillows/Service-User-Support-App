5abfbfc775e3803eb2d72a071a9d5f8ad77d2cc2
Stuart <47339755+BearintheWillows@users.noreply.github.com>
2023-03-29 07:29:45 +0100

    Added Registration DTOs

---
+++ susa.API/Auth/Entities/DTOs/Authentication/AUserForRegistrationDto.cs
@@ -0,0 +1,20 @@
+﻿namespace susa.API.Auth.Entities.DTOs;
+
+using System.ComponentModel.DataAnnotations;
+
+public class AppUserForRegistrationDto
+{
+	public string? FirstName { get; set; }
+	public string? LastName { get; set; }
+	
+	[Required(ErrorMessage = "Email is required")]
+	public string? Email { get; set; }
+	
+	[Required(ErrorMessage = "Password is required")]
+	[DataType(DataType.Password)]
+	public string? Password { get; set; }
+	
+	[Compare("Password", ErrorMessage = "Confirm Password is required")]
+	[DataType(DataType.Password)]
+	public string? ConfirmPassword { get; set; }
+}
---
+++ susa.API/Auth/Entities/DTOs/Responses/RegistrationResponseDto.cs
@@ -0,0 +1,7 @@
+﻿namespace susa.API.Auth.Entities.DTOs.Responses;
+
+public class RegistrationResponseDto
+{
+	public bool IsSuccessful { get; set; }
+	public IEnumerable<string>? Errors { get; set; }
+}
