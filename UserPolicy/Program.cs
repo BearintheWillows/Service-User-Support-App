0d490d985c299245076cae19f13f7369b9c8fa86
Stuart <47339755+BearintheWillows@users.noreply.github.com>
2023-04-06 20:50:26 +0100

    Added Serilog.

--- UserPolicy/Program.cs
+++ UserPolicy/Program.cs
@@ -1,9 +1,17 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Serilog;
+using Serilog.Events;
 using UserPolicy;
 using UserPolicy.Entities.Models;
 
 var builder = WebApplication.CreateBuilder( args );
+
+Log.Logger = new LoggerConfiguration()
+            .MinimumLevel.Debug()
+            .WriteTo.File( "Logs/log-.txt", rollingInterval: RollingInterval.Day )
+            .WriteTo.Console( restrictedToMinimumLevel: LogEventLevel.Information )
+            .CreateLogger();
 
 builder.Services.AddDbContext<AuthDbContext>( options =>
 	{
--- susa.API/Program.cs
+++ susa.API/Program.cs
@@ -1,6 +1,16 @@
 using Mapster;
+using Serilog;
+using Serilog.Events;
+using static Serilog.RollingInterval;
 
 var builder = WebApplication.CreateBuilder( args );
+
+Log.Logger = new LoggerConfiguration()
+            .MinimumLevel.Debug()
+            .WriteTo.File( "Logs/log-.txt", rollingInterval: RollingInterval.Day )
+            .WriteTo.Console( restrictedToMinimumLevel: LogEventLevel.Information )
+            .CreateLogger();
+
 
 // Add services to the container.
 
@@ -9,6 +19,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
 
 
 var app = builder.Build();
