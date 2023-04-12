8624b4f36793ec38f82d76ab44e45406e3087283
Stuart <47339755+BearintheWillows@users.noreply.github.com>
2023-04-12 20:17:59 +0100

    Added Ionic to Base Angular App.

--- susa.CLIENT/src/app/app.module.ts
+++ susa.CLIENT/src/app/app.module.ts
@@ -3,13 +3,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { IonicModule } from '@ionic/angular';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule, HttpClientModule
+    BrowserModule, HttpClientModule, IonicModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
