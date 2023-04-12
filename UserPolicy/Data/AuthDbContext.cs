6498d2541ec21b2b64e3539a1c70eda4ca14bea8
Stuart <47339755+BearintheWillows@users.noreply.github.com>
2023-04-12 22:59:23 +0100

    Started adding Profile Page

--- UserPolicy/Data/AuthDbContext.cs
+++ UserPolicy/Data/AuthDbContext.cs
@@ -13,7 +13,7 @@ public class AuthDbContext : IdentityDbContext<User>
 	protected override void OnModelCreating(ModelBuilder builder)
 	{
 		base.OnModelCreating( builder );
-		builder.ApplyConfiguration( new RoleConfiguration() );
+		builder.ApplyConfiguration( new RoleConfiguration() );	
 	}
 	
 }
--- susa.CLIENT/src/app/app.routes.ts
+++ susa.CLIENT/src/app/app.routes.ts
@@ -11,4 +11,8 @@ export const routes: Routes = [
     loadComponent: () =>
       import('./folder/folder.page').then((m) => m.FolderPage),
   },
+  {
+    path: 'profile',
+    loadComponent: () => import('./profiles/user-profile/user-profile.page').then( m => m.UserProfilePage)
+  },
 ];
