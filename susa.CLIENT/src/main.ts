ea47dbb1c07e645ee610bd7fa7276581ae08378b
Stuart <47339755+BearintheWillows@users.noreply.github.com>
2023-04-15 19:25:27 +0100

    Added error-handler.service.ts and added to main.ts

--- susa.CLIENT/src/main.ts
+++ susa.CLIENT/src/main.ts
@@ -6,7 +6,8 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
 import { environment } from './environments/environment';
-import { provideHttpClient } from '@angular/common/http';
+import {HTTP_INTERCEPTORS, provideHttpClient} from '@angular/common/http';
+import {ErrorHandlerService} from "./app/services/error-handler.service";
 
 if (environment.production) {
   enableProdMode();
@@ -15,9 +16,11 @@ if (environment.production) {
 bootstrapApplication(AppComponent, {
   providers: [
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    {provide: HTTP_INTERCEPTORS, useClass: ErrorHandlerService},
     importProvidersFrom(IonicModule.forRoot({})),
     provideRouter(routes),
     provideHttpClient(),
+
 
   ],
 });
